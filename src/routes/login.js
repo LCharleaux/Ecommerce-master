7f91a5c7f875f5206500efc0d79c1a8f466205a6
LCharleaux <luischarleaux12@gmail.com>
2022-10-31 11:29:17 -0300

    form refister arrumado

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controller/login.controller');
 
-router.post('/', loginController.registerUser);
+router.post('/register', loginController.registerUser);
 router.post('/', loginController.showHomePage);
 router.get('/', loginController.showLoginPage);
 
